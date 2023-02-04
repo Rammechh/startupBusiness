075934f599c6d3c60938cfb9278d57e7048de37f
Your Name <you@example.com>
2023-02-05 01:28:17 +0530

    commit

--- users/views.py
+++ users/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from .models import Profile
 
+#Dont use name login we going to use bulit-in 
+def loginPage(request):
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, 'users/login_register.html')
+
 def profiles(request):
     profiles = Profile.objects.all()
     context = {'profiles': profiles}
     return render(request, 'users/profiles.html', context)
+
+def userProfile(request, pk):
+    
+    profile = Profile.objects.get(id = pk)
+
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description = "")
+
+    context = {'profile' : profile, 'topSkills' : topSkills, 'otherSkills': otherSkills}
+    return render(request, 'users/user-profile.html', context)
