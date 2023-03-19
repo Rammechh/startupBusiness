1ae920e53b9255b1166d7a04f0f72bdfa56fb763
Your Name <you@example.com>
2023-03-20 02:05:31 +0530

    Bug Fixes

--- projects/views.py
+++ projects/views.py
@@ -37,11 +37,16 @@ def createProject(request):
     form = ProjectForm()
     profile = request.user.profile
     if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',', " ").split()
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
             project = form.save(commit = False)
             project.owner = profile
             project.save()
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name = tag)
+                project.tags.add(tag)
+
             return redirect('projects')
 
     context = {'form' : form}
@@ -53,9 +58,13 @@ def updateProject(request, pk):
     project = profile.project_set.get(id = pk)
     form = ProjectForm(instance=project)
     if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',', " ").split()
         form = ProjectForm(request.POST,request.FILES ,instance= project)
         if form.is_valid():
             form.save()
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name = tag)
+                project.tags.add(tag)
             return redirect('projects')
 
     context = {'form' : form}
